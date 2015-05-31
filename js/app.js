431acbcab3ebf72b2620f132e941625cacf78deb
Henry <henryw07@gmail.com>
2015-05-31 14:41:31 -0400

    Refactored common functionality out into a Character superclass

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+// Generic character
+var Character = function (xCoordiante, yCoordinate, spritePath) {
+    this.x = xCoordiante;
+    this.y = yCoordinate;
+    this.sprite = spritePath;
+};
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-}
+// Draw the enemy on the screen, required method for game
+Character.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Enemies our player must avoid
+var Enemy = function(xCoordiante, yCoordinate, spritePath) {
+    Character.call(this, xCoordiante, yCoordinate, spritePath);
+};
+Enemy.prototype = Object.create(Character.prototype);
+Enemy.prototype.consturctor = Enemy;
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -14,12 +23,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-}
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 // Now write your own player class
 // This class requires an update(), render() and
