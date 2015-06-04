19332b7949121abfc4544cdf2dc5764583690a04
Henry <henryw07@gmail.com>
2015-06-03 21:26:45 -0400

    Added logic to create Enemy characters

--- js/app.js
+++ js/app.js
@@ -2,6 +2,16 @@
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var player = new Player('images/char-boy.png');
+
+const MIN_ENEMIES = 5;
+const MAX_ENEMIES = 10;
+var numberOfEnemies = MIN_ENEMIES + (parseInt(Math.random()) % MAX_ENEMIES);
+
+var allEnemies = new Array(numberOfEnemies);
+allEnemies.forEach(function(value, index) {
+   allEnemies[index] = new Enemy('enemy-bug.png'); 
+});
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
