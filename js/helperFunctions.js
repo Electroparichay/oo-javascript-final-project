cae251837d7c7e4a4d97fc12a79d1cf1222b39bf
Henry <henryw07@gmail.com>
2015-06-03 21:30:41 -0400

    Added min() and randomInt() to helperFunctions.js

--- js/helperFunctions.js
+++ js/helperFunctions.js
@@ -1,4 +1,15 @@
 
+/* Returns the largest number in {numberArray}*/
 var max = function(numberArray) {
 	Math.max.apply(null, numberArray);
+};
+
+var min = function(numberArray) {
+	Math.min.apply(null, numberArray);
+};
+
+/* Returns a random number between {from} and {to}, inclusive.*/
+var randomInt = function(from, to) {
+	randomNumber = ( Math.random()* (max-min+1) ) + min;
+	return parseInt(randomNumber);
 };
