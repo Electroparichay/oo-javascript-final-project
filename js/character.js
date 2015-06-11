28f3f7656695d1e7248cd060806cae08715c1f4b
Henry <henryw07@gmail.com>
2015-06-11 01:35:44 -0400

    Added new methods to Character

--- js/character.js
+++ js/character.js
@@ -2,8 +2,18 @@
 var Character = function (position, spritePath) {
     this.x = position ? position.x : undefined;
     this.y = position ? position.y : undefined;
-    this.isOnScreen = true;
     this.sprite = spritePath;
+};
+
+//Returns true if enemy is within the canvas boundaries
+Character.prototype.isOnScreen = function() {
+    var canvasRectangle = engine.canvas.getBoundingClientRect();	
+    return this.x >= canvasRectangle.left && this.x <= canvasRectangle.right &&
+        this.y <= canvasRectangle.bottom && this.y >= canvasRectangle.top;
+};
+
+Character.prototype.getBoundingClientRect = function() {
+	throw new Error('Not yet implemented');
 };
 
 // Draw the character on the screen
