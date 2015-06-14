8326c2728d36b937717a0051ef89a61c882fa345
Henry <henryw07@gmail.com>
2015-06-14 14:07:02 -0400

    Fixed movement bug for Enemy

--- js/enemy.js
+++ js/enemy.js
@@ -6,34 +6,26 @@ var Enemy = function(spritePath) {
 		  Y coordinate is selected by randomly choosing a grass tile row and then getting the character's
 		  would-be position on that row.
 		*/
-		var canvasRectangle = canvas.getBoundingClientRect();
-		var randomX =  randomInt(0, canvasRectangle.left);
-	    var topRow = max(GRASS_ROWS);
-	    var bottomRow = min(GRASS_ROWS);
+		var canvasRectangle = engine.canvas.getBoundingClientRect();
+		var xPosition = 0;
+	    var topRow = min(Engine.STONE_ROWS);
+	    var bottomRow = max(Engine.STONE_ROWS);
 	    var randomRow = randomInt(topRow, bottomRow);
 		var randomY = PositionFunctions.getCharacterPosition(randomRow, 0).y;
 
-		return new Position(randomX, randomY);
+		return new Position(xPosition, randomY);
 	}
 
 	var position = getRandomCoordinates();
     Character.call(this, position, spritePath);
+	this.speed = randomInt(100, 250);
 };
 
 Enemy.prototype = Object.create(Character.prototype);
 Enemy.prototype.constructor = Enemy;
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    
-};
+var canvasRight = Engine.NUM_COLS * Engine.TILE_SIZE.width;
 
-//Returns true if enemy is within the canvas boundaries
-Enemy.prototype.isOnScreen = function() {
-    return this.x >= BOUNDARIES.left && this.x <= BOUNDARIES.right &&
-        this.y <= BOUNDARIES.down && this.y >= BOUNDARIES.up;
+Enemy.prototype.update = function(dt) {
+	this.x = (this.x + dt * this.speed) % canvasRight;
 };
