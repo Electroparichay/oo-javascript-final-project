1500fb3b92cb5e6ffee76270802696594b12bca6
Henry Williams <henryw07@gmail.com>
2015-06-20 15:56:41 -0400

    Cleaned up enemy.js

--- js/enemy.js
+++ js/enemy.js
@@ -1,11 +1,6 @@
 var Enemy = function(spritePath) {
-	function getRandomCoordinates() {
-		/*The Enemy's position is computed by randomly selecting a coordinate on the canvas.
-		  X coordinate is selected by randomly choosing a point between the leftmost point of the screen (0)
-		  and the x coordinate of the leftmost point of the canvas.
-		  Y coordinate is selected by randomly choosing a grass tile row and then getting the character's
-		  would-be position on that row.
-		*/
+	function getRandomPosition() {
+		/*The position is computed by randomly selecting a grass row on the on the canvas.*/
 		var canvasRectangle = engine.canvas.getBoundingClientRect();
 		var xPosition = 0;
 	    var topRow = min(Engine.STONE_ROWS);
@@ -16,7 +11,7 @@ var Enemy = function(spritePath) {
 		return new Position(xPosition, randomY);
 	}
 
-	var position = getRandomCoordinates();
+	var position = getRandomPosition();
     Character.call(this, position, spritePath);
 	this.speed = randomInt(100, 250);
 };
